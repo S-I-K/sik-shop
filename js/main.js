dc4316bf53e8d37df1a8e3e254a56e6039358282
SIK <daesik99@naver.com>
2023-05-07 17:23:31 +0900

    add/ collection part

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(()=>{
+    $('a').click(function(e){
+        e.preventDefault();
+        console.log('clicked');
+    });
+
+    /* intro slide */
     var swiper = new Swiper("#intro-slide", {
         slidesPerView: "auto",
         loop: true,
         autoplay: {
-            delay: 2000,
+            delay: 4000,
             disableOnInteraction: false,
         },
         pagination: {
@@ -14,5 +20,11 @@ $(()=>{
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+    });
+
+    $('.collection-text-box > .btn').mouseenter(function(e){
+        $('.more-btn').attr('src', './img/icon-arrow-white.svg');
+    }).mouseleave(function(e){
+        $('.more-btn').attr('src', './img/icon-arrow.svg');
     });
 });
