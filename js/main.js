2e4c42241f02ac048854e425240df47b35c62e19
sik <daesik99@naver.com>
2023-05-09 11:08:35 +0900

    update/ faq & news part

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ $(()=>{
         contents.eq(tabI).addClass('active');
     });
 
+    /* notice accordion */
+    // faq-q를 클릭 했을 때 자식요소인 faq-a가 아래로 슬라이드 되어서 보여져야 함
+    // faq-q에 show가 있으면 faq-a 슬라이드
+    // faq-q 클릭 시 해당요소에만 show 추가, 다른 faq-q들은 show 삭제
+    $('.faq-q').click(function(e){
+        $(this).toggleClass('show');
+        $(this).siblings().removeClass('show');
+        if($(this).hasClass('show')){
+            $(this).find('.faq-a').stop().slideDown(400);
+            $('.faq-q').not($(this)).find('.faq-a').stop().slideUp(400);
+        }else{
+            $(this).find('.faq-a').stop().slideUp(400);
+        }
+    });
+
     /* intro slide */
     var swiper = new Swiper("#intro-slide", {
         slidesPerView: "auto",
